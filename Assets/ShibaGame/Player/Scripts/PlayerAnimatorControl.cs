0b3e473b40fdd0e6995eead51787b4482a52e42b
Andrew Hoyt <andrew@chillhammer.com>
2019-09-15 11:02:48 -0400

    Rigidbody, Leap Implemented

--- Assets/ShibaGame/Player/Scripts/PlayerAnimatorControl.cs
+++ Assets/ShibaGame/Player/Scripts/PlayerAnimatorControl.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Controls animations and other movement
 public class PlayerAnimatorControl : MonoBehaviour
 {
+
+    public bool Leaping { get { return anim.GetBool("Leap"); } set { anim.SetBool("Leap", value); } }
+    public float Movement { get { return Mathf.Clamp01(Mathf.Sqrt(inputHor * inputHor + inputVert * inputVert)); } }
+
     public CameraPlayerControl cameraControl;
 
     public float leanLerpSpeed = 2.0f; // Animation speed to switch from leans
-    public float turnSpeedFollowingPlayer = 270.0f;
+    public float turnSpeedFollowingPlayer = 290.0f;
     public float turnSpeedFreeCamera = 180.0f;
 
     private Animator anim;
 
     private float lean = 0.0f;
+    private float inputHor;
+    private float inputVert;
 
     // Start is called before the first frame update
     void Start()
@@ -28,13 +35,13 @@ public class PlayerAnimatorControl : MonoBehaviour
 
     void OnAnimatorMove()
     {
-        float inputVert = Input.GetAxis("Vertical");
-        float inputHor = Input.GetAxis("Horizontal");
+        inputVert = Input.GetAxis("Vertical");
+        inputHor = Input.GetAxis("Horizontal");
 
         // Can't Move Backwards if Camera Free
         if (!cameraControl.followPlayer) inputVert = Mathf.Clamp01(inputVert);
 
-        float movement = Mathf.Sqrt(inputHor * inputHor + inputVert * inputVert);
+        float movement = Movement;
 
         float turnSpeed = turnSpeedFreeCamera;
 
@@ -52,9 +59,13 @@ public class PlayerAnimatorControl : MonoBehaviour
             // Change Turning To Turn to Input Direction
             inputHor = Mathf.Clamp(turnAngle, -1.0f, 1.0f);
 
+            
+
             // Modify Turn Speed
             turnSpeed = turnSpeedFollowingPlayer;
         }
+        // Air Slow Turn
+        turnSpeed *= (Leaping ? 0.1f : 1.0f);
 
 
         lean = Mathf.Lerp(lean, inputHor, 2.0f * Time.deltaTime);
@@ -76,4 +87,5 @@ public class PlayerAnimatorControl : MonoBehaviour
         anim.SetLayerWeight(anim.GetLayerIndex("Lean"), Mathf.Abs(lean) * 1.0f);
         
     }
+
 }
---
+++ Assets/ShibaGame/Player/Scripts/PlayerPhysicsControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPhysicsControl : MonoBehaviour
+{
+    public PlayerAnimatorControl animControl;
+
+    public float LeapForceTime = 1.0f;
+    private float leapTimer = 0.0f;
+
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Leap
+        if (!animControl.Leaping && Input.GetAxis("Fire1") > 0)
+        {
+            animControl.Leaping = true;
+            Vector3 leapForce = transform.up * 4f + transform.forward * (25.0f + animControl.Movement * 2.0f);
+            rb.velocity = leapForce;
+            // rb.AddForce(leapForce, ForceMode.VelocityChange);
+            leapTimer = LeapForceTime;
+        }
+
+        // Leap Force
+        leapTimer = Mathf.Max(leapTimer - Time.deltaTime, 0.0f);
+        if (animControl.Leaping && leapTimer > 0.0f)
+        {
+            rb.AddForce(transform.forward * 1000.0f * Time.deltaTime, ForceMode.Force);
+        } else if (animControl.Leaping)
+        {
+            rb.AddForce(-Vector3.up * 20.0f, ForceMode.Acceleration);
+        }
+
+        // Exit Leap
+        if (rb.velocity == Vector3.zero)
+        {
+            animControl.Leaping = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        animControl.Leaping = false;
+    }
+}
