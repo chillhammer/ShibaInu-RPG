97796edbc90d435291ef8e81b5b8b444d4737642
Brandon Shockley <bshoc@bellsouth.net>
2019-09-23 13:50:20 -0400

    Added damage interface and .vscode to gitignore

---
+++ Assets/ShibaGame/General/Scripts/IDamageReceiver.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public enum ImpactType
+{
+    NONE,
+    LIGHT,
+    HEAVY,
+}
+
+public struct Damage
+{
+    ImpactType impactType;
+    int amount;
+    Vector3 direction;
+}
+
+public interface IDamageReceiver
+{
+    void TakeDamage(Damage damage);
+}
