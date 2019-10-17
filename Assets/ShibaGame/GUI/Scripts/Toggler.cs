5b3aeb9fabcfdbbbb09c52e5b7dc49140b115fd9
Brandon Shockley <bshoc@bellsouth.net>
2019-10-17 11:52:20 -0400

    Added title screen

---
+++ Assets/ShibaGame/GUI/Scripts/Toggler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toggler : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject target = null;
+
+    public void Enable()
+    {
+        target.SetActive(true);
+    }
+
+    public void Disable()
+    {
+        target.SetActive(false);
+    }
+}
