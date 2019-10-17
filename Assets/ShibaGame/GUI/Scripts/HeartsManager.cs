d471e6313b70c1487c9c723e97e6fbf3b5fc28b5
Brandon Shockley <bshoc@bellsouth.net>
2019-10-17 16:05:27 -0400

    Added health bar UI and 3rd party assets list

---
+++ Assets/ShibaGame/GUI/Scripts/HeartsManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartsManager : MonoBehaviour
+{
+    [SerializeField]
+    private uint heartImageWidth = 20;
+    [SerializeField]
+    private PlayerHealth playerHealth = null;
+
+    private Slider s;
+
+    void Start()
+    {
+        s = GetComponent<Slider>();
+        s.maxValue = playerHealth.MaxHealth;
+
+        //Size to match the max health
+        RectTransform rTrans = GetComponent<RectTransform>();
+        rTrans.offsetMax = new Vector2(heartImageWidth * playerHealth.MaxHealth, rTrans.offsetMax.y);
+
+        playerHealth.OnHealthChange += OnHealthChange;
+    }
+
+    private void OnHealthChange(int oldVal, int newVal)
+    {
+        s.value = newVal;
+    }
+}
