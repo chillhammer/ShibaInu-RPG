d74c0465dc52bdf1551250ac6a5ca5c014a4fb44
Brandon Shockley <bshoc@bellsouth.net>
2019-10-18 09:14:29 -0400

    Added pause menu

---
+++ Assets/ShibaGame/GUI/Scripts/PauseToggler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseToggler : MonoBehaviour
+{
+    private CanvasGroup cg;
+    private CursorLockMode prevCursorLockState;
+    private bool prevCursorVisible;
+
+    void Start()
+    {
+        cg = GetComponent<CanvasGroup>();
+        ToggleCanvasGroup(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab)) {
+            if (cg.alpha == 1) {
+                ToggleCanvasGroup(false);
+                Time.timeScale = 1;
+                Cursor.lockState = prevCursorLockState;
+                Cursor.visible = prevCursorVisible;
+            } else {
+                ToggleCanvasGroup(true);
+                Time.timeScale = 0;
+                prevCursorLockState = Cursor.lockState;
+                prevCursorVisible = Cursor.visible;
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+        }
+    }
+
+    private void ToggleCanvasGroup(bool shouldEnable)
+    {
+        cg.alpha = shouldEnable ? 1 : 0;
+        cg.blocksRaycasts = shouldEnable;
+        cg.interactable = shouldEnable;
+    }
+}
