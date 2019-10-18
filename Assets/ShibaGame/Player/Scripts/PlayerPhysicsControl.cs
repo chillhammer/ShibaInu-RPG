08fbe91d6e673a8957ab949a98c34ec991f45322
Andrew Hoyt <andrew@chillhammer.com>
2019-10-18 01:05:32 -0400

    Slightly better jump, auto jump

--- Assets/ShibaGame/Player/Scripts/PlayerPhysicsControl.cs
+++ Assets/ShibaGame/Player/Scripts/PlayerPhysicsControl.cs
@@ -9,16 +9,24 @@ public class PlayerPhysicsControl : MonoBehaviour
     public float LeapForceTime = 1.0f;
     private float leapTimer = 0.0f;
 
+    public LayerMask checkGroundMask;
+
     public Rigidbody rb;
+    private CapsuleCollider collider;
+
+    private bool wasOnGround = true;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        collider = GetComponent<CapsuleCollider>();
     }
 
     void Update()
     {
+        bool onGround = CheckOnGround();
+
         // Leap
-        if (!animControl.Leaping && Input.GetAxis("Fire1") > 0)
+        if (!IsLeaping() && (Input.GetButtonDown("Jump") || (wasOnGround && !onGround)))
         {
             animControl.Leaping = true;
             Vector3 leapForce = transform.up * 10f + transform.forward * (5.0f + animControl.Movement * 10.0f);
@@ -27,19 +35,24 @@ public class PlayerPhysicsControl : MonoBehaviour
             // rb.AddForce(leapForce, ForceMode.VelocityChange);
             leapTimer = LeapForceTime;
         }
+        wasOnGround = onGround;
 
         // Leap Force
         leapTimer = Mathf.Max(leapTimer - Time.deltaTime, 0.0f);
-        if (animControl.Leaping && leapTimer > 0.0f)
+        if (IsLeaping())
         {
-            rb.AddForce(transform.forward * 10.0f * Time.deltaTime, ForceMode.Force);
-        } else if (animControl.Leaping)
-        {
-            rb.AddForce(-Vector3.up * 50.0f, ForceMode.Acceleration);  
+            if (leapTimer > 0.0f)
+            {
+                rb.AddForce(transform.forward * 10.0f * Time.deltaTime, ForceMode.Force);
+            }
+            else
+            {
+                rb.AddForce(Vector3.down * 50.0f, ForceMode.Acceleration);
+            }
         }
 
         // Exit Leap
-        if (rb.velocity == Vector3.zero)
+        if (rb.velocity == Vector3.zero && onGround)
         {
             animControl.Leaping = false;
         }
@@ -48,5 +61,56 @@ public class PlayerPhysicsControl : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         animControl.Leaping = false;
+    }
+
+    bool IsLeaping()
+    {
+        return animControl.Leaping;
+    }
+
+    bool CheckOnGround()
+    {
+        Vector3 center = transform.position;
+        Vector3 extents = collider.bounds.extents;
+
+        RaycastHit hitInfo;
+        Ray ray = new Ray(center, Vector3.down);
+
+        Vector3 slimExtents = extents;
+        slimExtents.y = 0.1f;
+
+        if (Physics.BoxCast(center, slimExtents, Vector3.down, Quaternion.identity, extents.y + 0.2f, checkGroundMask))
+        {
+            return true;
+        }
+
+
+        if (Physics.Raycast(ray, out hitInfo, collider.bounds.extents.y + 0.1f, checkGroundMask))
+        {
+            return true; // Left in here for debugging purposes
+        }
+
+        return false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Vector3 center = transform.position + collider.center;
+        Vector3 extents = collider.bounds.extents;
+
+        //Gizmos.color = Color.red;
+        //Gizmos.DrawWireCube(center, extents);
+        //Gizmos.color = Color.blue;
+        //Gizmos.DrawWireCube(center + Vector3.down * 0.1f, extents);
+
+        Vector3 slimExtents = extents;
+        slimExtents.y = 0.1f;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(center, slimExtents);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(center + Vector3.down * (0.2f + extents.y), slimExtents);
+
+        Gizmos.DrawLine(center, center + Vector3.down * (collider.bounds.extents.y + 0.1f));
     }
 }
