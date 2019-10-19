582e00e0557631b6dee1c61ad58ab3ca6fdf5959
Xueyu <xueyuw@gatech.edu>
2019-10-19 13:00:19 -0400

    arrange files

---
+++ Assets/ShibaGame/Environment/Script/FallenTree.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallenTree : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.attachedRigidbody)
+        {
+            anim.SetBool("FallenTree", true);
+
+        }
+    }
+    void OnTriggerExit(Collider c)
+    {
+        if (c.attachedRigidbody)
+        {
+            anim.SetBool("FallenTree", false);
+
+        }
+    }
+
+
+}
