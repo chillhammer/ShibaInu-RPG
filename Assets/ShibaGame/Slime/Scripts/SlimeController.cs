0872ec18c02e624e3521cba45e93123af420e8c9
joeygonzalesdones <33211226+joeygonzalesdones@users.noreply.github.com>
2019-10-21 19:55:06 -0400

    Implemented TakeDamage() function in SlimeController

--- Assets/ShibaGame/Slime/Scripts/SlimeController.cs
+++ Assets/ShibaGame/Slime/Scripts/SlimeController.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlimeController : MonoBehaviour
+public class SlimeController : MonoBehaviour, IDamageReceiver
 {
     [SerializeField]
     private int attackDamage = 1;
+    [SerializeField]
+    private int health = 2;
 
     private Animator anim;
+    private Rigidbody rb;
 
     void Start()
     {
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
     }
 
     private void OnCollisionEnter(Collision coll)
@@ -40,6 +44,28 @@ public class SlimeController : MonoBehaviour
         if (dr != null)
         {
             anim.SetBool("SlimeHop", false);
+        }
+    }
+
+    public void TakeDamage(Damage damage)
+    {
+        health -= damage.Amount;
+
+        Vector3 dir = damage.Direction;
+        switch (damage.ImpactType)
+        {
+            case ImpactType.LIGHT:
+                dir = dir * 15.0f + Vector3.up * 15.0f;
+                rb.AddForce(dir, ForceMode.Impulse);
+                break;
+        }
+
+        if (health <= 0)
+        {
+            //TODO: Add death animation or something
+            LootDropper ld = gameObject.GetComponent<LootDropper>();
+            Destroy(gameObject);
+            ld.DropLoot(transform.position, transform.rotation);
         }
     }
 }
