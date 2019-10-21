6e68fbde15fdbd624c6acc38afee7440a8f85065
joeygonzalesdones <33211226+joeygonzalesdones@users.noreply.github.com>
2019-10-20 20:16:36 -0400

    Added Slime prefab, model, animations, and controller script

---
+++ Assets/ShibaGame/Slime/Scripts/SlimeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeController : MonoBehaviour
+{
+    [SerializeField]
+    private int attackDamage = 1;
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        Collider c = coll.collider;
+        IDamageReceiver dr = c.GetComponent<IDamageReceiver>();
+        if (dr != null)
+        {
+            Damage damage = new Damage(ImpactType.LIGHT, attackDamage, (c.transform.position - transform.position).normalized);
+            dr.TakeDamage(damage);
+        }
+    }
+
+    private void OnTriggerEnter(Collider c)
+    {
+        IDamageReceiver dr = c.GetComponent<IDamageReceiver>();
+        if (dr != null)
+        {
+            anim.SetBool("SlimeHop", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider c)
+    {
+        IDamageReceiver dr = c.GetComponent<IDamageReceiver>();
+        if (dr != null)
+        {
+            anim.SetBool("SlimeHop", false);
+        }
+    }
+}
