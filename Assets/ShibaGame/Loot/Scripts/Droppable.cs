276acdd30c0a4bdb672a5f9b6fd81b3f721774ae
rosie <rosieablair@gmail.com>
2019-10-21 03:43:36 -0400

    Fixed some stuff & imported sound fx for loot & spiky bush.

--- Assets/ShibaGame/Loot/Scripts/Droppable.cs
+++ Assets/ShibaGame/Loot/Scripts/Droppable.cs
@@ -8,6 +8,7 @@ public class Droppable : MonoBehaviour
 	public void Drop(Vector3 location, Quaternion angle)
 	{
 		location.y = 1.7166f;
+		location.z += 2.0f;
 		transform.position = location;
 		Instantiate(gameObject, location, angle);
 		Debug.Log("Loot instantiated at enemy location: " + location.ToString("F4"));
