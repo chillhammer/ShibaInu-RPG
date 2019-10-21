994899a43fea2254f042e3ebc468c07a92eb370e
rosie <rosieablair@gmail.com>
2019-10-20 23:23:54 -0400

    Added animation for loot to drop at location of enemy when destroyed
    Added a bounce-like animation of item dropping on the ground. Made the item instantiate at the location of the enemy's death.

---
+++ Assets/ShibaGame/Loot/Scripts/Droppable.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Droppable : MonoBehaviour
+{
+
+	public void Drop(Vector3 location, Quaternion angle)
+	{
+		location.y = 1.7166f;
+		transform.position = location;
+		Instantiate(gameObject, location, angle);
+		Debug.Log("Loot instantiated at enemy location: " + location.ToString("F4"));
+	}
+}
