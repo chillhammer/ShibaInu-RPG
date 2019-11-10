58406faf06a4ad06f2741c9b3f7d46c2f9b4dbaa
Xueyu <xueyuw@gatech.edu>
2019-11-10 17:14:44 -0500

    add fallen tree damage

---
+++ Assets/ShibaGame/Environment/Script/FallenTreeDamage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallenTreeDamage : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+    public int treeDamage = 10;
+    [SerializeField] private AudioClip pierceSound;
+
+    private SoundModulator sm;
+
+    void Start()
+    {
+        sm = GetComponent<SoundModulator>();
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Fallen Tree"))
+        {
+            IDamageReceiver dr = c.GetComponent<IDamageReceiver>();
+            if (dr != null)
+            {
+                Damage damage = new Damage(ImpactType.LIGHT, treeDamage, (c.transform.position - transform.position).normalized);
+                dr.TakeDamage(damage);
+                sm.PlayModClip(pierceSound);
+            }
+        }
+    }
+
+}
