39de2fef1d56286f0b975ae73af5c617b4c1eb4a
Brandon Shockley <bshoc@bellsouth.net>
2019-12-01 19:29:22 -0500

    Added victory state and rotating bone

---
+++ Assets/ShibaGame/Loot/Scripts/Bone.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bone : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject victoryMenu = null;
+
+    private float spinSpeed = 90;
+    private float bobSpeed = 1;
+    private float bobAmplitude = 0.2f;
+    private bool idling = true;
+    private float originY;
+
+    private CanvasGroup cg;
+
+    void Start()
+    {
+        cg = victoryMenu.GetComponent<CanvasGroup>();
+
+        originY = transform.position.y;
+    }
+
+    void Update()
+    {
+        if (idling) {
+            transform.Rotate(0, spinSpeed * Time.deltaTime, 0);
+            Vector3 newPos = transform.position;
+            newPos.y = originY + Mathf.Sin(Time.time * bobSpeed) * bobAmplitude;
+            transform.position = newPos;
+        }
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        cg.alpha = 1;
+        cg.blocksRaycasts = true;
+        cg.interactable = true;
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
